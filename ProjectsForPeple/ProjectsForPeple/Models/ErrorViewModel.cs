a50646b526a72838a8af57dcf941934771f18c1a
bombel89 <damian.gburzyn@gmail.com>
2020-02-29 00:05:59 +0100

    add solution

---
+++ ProjectsForPeple/ProjectsForPeple/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ProjectsForPeple.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
