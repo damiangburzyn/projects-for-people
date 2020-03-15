c3edba530135af202fe09d80139d090996f65d36
bombel89 <damian.gburzyn@gmail.com>
2020-03-15 20:57:55 +0100

    New inital solution as web api

--- ProjectsForPeople/ProjectsForPeople/Startup.cs
+++ ProjectsForPeople/ProjectsForPeople/Startup.cs
@@ -26,6 +26,8 @@ namespace ProjectsForPeople
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            //connect middleware
+            services.AddSpaStaticFiles(options => options.RootPath = "client-app/dist");
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,6 +36,11 @@ namespace ProjectsForPeople
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.  
+                app.UseHsts();
             }
 
             app.UseHttpsRedirection();
@@ -45,6 +52,18 @@ namespace ProjectsForPeople
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+            
+            // use middleware and launch server for Vue  
+            app.UseSpaStaticFiles();
+            app.UseSpa(spa =>
+            {
+                spa.Options.SourcePath = "client-app";
+                if (env.IsDevelopment())
+                {
+
+                    spa.UseVueDevelopmentServer();
+                }
             });
         }
     }
---
+++ ProjectsForPeople/ProjectsForPeople/client-app/src/components/Forum/AddComment.vue
@@ -0,0 +1,24 @@
+<template>
+    <v-text-field :auto-grow="true" :value="Message" :Placeholder="Placeholder"  :single-line="false" :solo="false" :rounded="rounded" :outlined="true"></v-text-field>
+
+
+</template>
+
+
+
+
+<script lang="ts">
+    import Vue from 'vue'
+    interface IAddComment {
+        Message: string,
+        Placeholder: string
+    }
+    export default Vue.extend({
+        name: 'AddComment',
+        data(): IAddComment {
+            return {
+                Message: "To jest Message",
+                Placeholder:"Placeholder"
+            }
+        }
+  })</script>
